4cc45e6dd51ae628ae5a162043ad5dcded465cc0
carlosalexandredevv <alexfilho062005@gmail.com>
2026-01-12 21:56:37 -0300

    Refactor Home component layout by consolidating DatePicker instances for improved responsiveness. Update PrismaClient import path for consistency with the latest Prisma package structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,26 +39,24 @@ export default async function Home({
           <p className="text-paragraph-medium-size text-content-secondary">
             Aqui você pode ver todos os clientes e serviços agendados para hoje.
           </p>
-          <div className="hidden md:flex item-center gap-4 mt-4">
-            <DatePicker />
-          </div>
-          <div className="mt-3 mb-8 md:hidden">
-            <DatePicker />
-          </div>
         </div>
+
+        <div className="hidden md:flex item-center gap-4">
+          <DatePicker />
+        </div>
+      </div>
+
+      <div className="mt-3 mb-8 md:hidden">
+        <DatePicker />
       </div>
 
       <div className="pb-24 md:pb-0">
         {periods.map((period, index) => (
-          <PeriodSection key={index} period={period} />
+          <PeriodSection period={period} key={index} />
         ))}
       </div>
-      <div
-        className={cn(
-          'fixed bottom-0 left-0 right-0 flex justify-center bg-[#23242C] py-[18px] px-6 md:right-6',
-          'md:left-auto md:top-auto md:w-auto md:bg-transparent',
-        )}
-      >
+
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center bg-[#23242C] py-[18px] px-6 md:bottom-6 md:right-6 md:left-auto md:top-auto md:w-auto md:bg-transparent md:p-0">
         <AppointmentForm>
           <Button variant="brand">Novo Agendamento</Button>
         </AppointmentForm>
